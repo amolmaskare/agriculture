acbd46053d305482e7089ca004b274d6b1be9996
Amol Sartajsingh Maskare <amolmaskare1@gmail.com>
2024-04-11 18:45:17 +0530

    first commit with all functionality

---
+++ contact.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>agricultural</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/global.css" rel="stylesheet">
+	<link href="css/contact.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+   <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	
+	
+	 
+
+  </head>
+<body>
+<section id="header_main">
+ <div class="container">
+  <div class="row">
+   <div class="col-sm-12 space_all">
+	<div class="col-sm-3 space_all">
+	 <div class="header_main_1">
+	  <a href="index.html"><i class="fa fa-tree"></i><small>Techkisan Farming</small></a>
+	 </div>
+	</div>
+	<div class="col-sm-9 space_all">
+	 <div class="header_main_2 clearfix">
+	  <div class="col-sm-4 space_all">
+	   <div class="header_main_3 clearfix">
+	    <div class="col-sm-2">
+		 <div class="header_main_4">
+		  <i class="fa fa-map-marker"></i>
+		 </div>
+		</div>
+		<div class="col-sm-10">
+		 <div class="header_main_5">
+		  <p> Goutam Nagar, Bajpai Ward,
+			Murri, Gondia, Maharashtra 441614</p>
+		 </div>
+		</div>
+	   </div>
+	  </div>
+	  <div class="col-sm-4 space_all">
+	   <div class="header_main_3 clearfix">
+	    <div class="col-sm-2">
+		 <div class="header_main_4">
+		  <i class="fa fa-mobile"></i>
+		 </div>
+		</div>
+		<div class="col-sm-10">
+		 <div class="header_main_5">
+			<p>79720 25213
+				<br />
+			   <a href="#">info@techkisan.in</a></p>
+		 </div>
+		</div>
+	   </div>
+	  </div>
+	  <div class="col-sm-4 space_all">
+	   <div class="header_main_3 clearfix">
+	    <div class="col-sm-2">
+		 <div class="header_main_4">
+		  <i class="fa fa-clock-o"></i>
+		 </div>
+		</div>
+		<div class="col-sm-10">
+		 <div class="header_main_5">
+		  <p>6 hours - 21 hours <br/>
+             *Weekends working</p>
+		 </div>
+		</div>
+	   </div>
+	  </div>
+	 </div>	 
+	</div>
+   </div>
+  </div>
+ </div>
+</section>
+<section id="header" class="cd-secondary-nav" class="clearfix">
+ <nav class="navbar navbar-default" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#dropdown-thumbnail-preview">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.html"><i class="fa fa-tree"></i>AGRICULTURAL</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="dropdown-thumbnail-preview">
+      <ul class="nav navbar-nav">
+        <li><a href="index.html">Home</a></li>
+		<li><a href="about.html">About</a></li>
+		<li><a href="smart_farming.html">Smart Farming</a></li>
+        <li><a href="shop.html">E-Store</a></li>
+		<li><a href="organic_farming.html">Organic Farming</a></li>
+		<li><a href="plantation_farming.html">Plantation Farming</a></li>
+		<li class="active"><a href="contact.php">Contact</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav> 
+</section>
+<section id="contact_main">
+ <div class="contact_main_3 clearfix">
+  <div class="container">
+  <div class="row">
+   <div class="contact_main_1 clearfix">
+    <div class="col-sm-6">
+	 <div class="contact_main_2">
+	  <h2>CONTACT</h2>
+	 </div>
+	</div>
+	<div class="col-sm-6">
+	 <div class="contact_main_2">
+	  <ul>
+	       <li>Home</li>
+		   <li>!</li>
+		   <li>Contact</li>
+	  </ul>
+	 </div>
+	</div>
+   </div>
+  </div>
+ </div>
+ </div>
+</section>
+<section id="contact">
+ <div class="container">
+  <div class="row">
+   <div class="contact_1">
+    <h2>GET IN TOUCH</h2>
+	<p>Contact us by email, phone or through our web form below.</p>
+   </div>
+   <div class="contact_2 clearfix">
+    <div class="col-sm-4">
+	 <div class="contact_3">
+	   <ul>
+	        <li><i class="fa fa-map-marker"></i>Address: Goutam Nagar, Bajpai Ward, Murri, Gondia, Maharashtra 441614</li>
+	  </ul>
+	 </div>
+	 <div class="contact_6">
+	  <h3>Booking Enquiries</h3>
+	  <p>Any enquiry you want contact usb or in writing by or on behalf of a prospective Client in relation to a potential Booking.</p>
+	 </div>
+	</div>
+	<div class="col-sm-4">
+	 <div class="contact_4">
+	   <ul>
+	        <li><i class="fa fa-phone"></i>Phones: <a href="#">79720 25213</a></li>
+	  </ul>
+	 </div>
+	 <div class="contact_6">
+	  <h3>Post Booking Questions</h3>
+	  <p>There shall be no personal communication enclosed or written up on a book packet. It shall not contain any paper money, postage or other stamps, Cheque​ etc Exception: It may contain stamped self addressed post card or letter or wrapper.
+		
+	  </p>
+	 </div>
+	</div>
+	<div class="col-sm-4">
+	 <div class="contact_5">
+	   <ul>
+	        <li><i class="fa fa-envelope"></i><a href="#"> info@techkisan.in/a></li> 
+	  </ul>
+	 </div>
+	 <div class="contact_6">
+	  <h3>Payment Queries</h3>
+	  <p>The Payment Query is an optional step in the workflow in which merchants can allow their customers to select the desired payment method dynamically.
+
+	  </p>
+	 </div>
+	</div>
+   </div>
+  </div>
+ </div>
+</section>
+<section id="exercise">
+ <div class="container">
+  <div class="row">
+   <div class="col-sm-12">
+    <div class="col-sm-6">
+	  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3635689.793072721!2d78.6421709625!3d27.141236999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1531201517392 " height="350" style="border:0; width:100%;" allowfullscreen=""></iframe>
+	</div>
+	<div class="col-sm-6">
+	 <div class="exercise_3">
+	  <h2>Sed Augue</h2>
+	 </div>
+	 <div class="col-sm-12 space_all">
+	  <div class="col-sm-4 space_all">
+	   <div class="exercise_4">
+	    <input type="text" class="form-control" placeholder="Name">
+	   </div>
+	  </div>
+	  <div class="col-sm-4 space_all">
+	   <div class="exercise_4">
+	    <input type="text" class="form-control" placeholder="Email">
+	   </div>
+	  </div>
+	  <div class="col-sm-4 space_all">
+	   <div class="exercise_4">
+	    <input type="text" class="form-control" placeholder="Phone">
+	   </div>
+	  </div>
+	 </div>
+	 <div class="col-sm-12 space_all">
+	  <div class="exercise_5">
+	   <textarea placeholder="Message" class="form-control"></textarea>
+	  </div>
+	 </div>
+	 <div class="col-sm-12 space_all">
+	  <div class="exercise_6">
+	   <a href="#" class="button_1">SEND MESSAGE</a>
+	  </div>
+	 </div>
+	</div>
+   </div>
+  </div>
+ </div>
+</section>
+<form action="connect.php" method="post">
+	<h3>Book your first visit today</h3>
+
+	<div class="inputBox">
+		<input type="text" placeholder="Your name" value="<?php if (isset($_POST['your_name'])) echo $_POST['your_name']?>"  name="your_name">
+	</div>
+
+	<div class="inputBox">
+		<input type="email" placeholder="Your email"value="<?php if (isset($_POST['email'])) echo $_POST['email']?>" name="email">
+	</div>
+
+	<div class="inputBox">
+		<input type="text" placeholder="Subject"value="<?php if (isset($_POST['subject'])) echo $_POST['subject']?>" name="subject">
+	</div>
+
+	<div class="inputBox">
+		<input type="text" placeholder="Message"value="<?php if (isset($_POST['message'])) echo $_POST['message']?>" name="message">
+	</div>
+
+	<!-- <input type="submit" value="Send a Message" class="btn"> -->
+	<div class="center-btn">   
+		<button class="btn" name="submit" value="submit">Send a Message</button>
+	</div>
+ 
+</form>
+<section id="footer">
+ <div class="footer_inner clearfix">
+   <div class="container">
+  <div class="row">
+   <div class="col-sm-12">
+    <div class="col-sm-3">
+	 <div class="footer_1">
+	  <h3>AGRICULTURAL</h3>
+	  <p>Agriculture is the art and science of cultivating the soil, growing crops and raising livestock. It includes the preparation of plant and animal products for people to use and their distribution to markets. </p>
+	 </div>
+	</div>
+	<div class="col-sm-3">
+	 <div class="footer_2">
+	  <h3>MAURIS MASSA</h3>
+	  <ul>
+		<li><i class="fa fa-map-marker"></i> Goutam Nagar, Bajpai Ward, Murri, Gondia, Maharashtra 441614</li>
+		<li><i class="fa fa-phone"></i> Phone: 79720 25213</li>
+		<li><i class="fa fa-envelope"></i> <a href="#">info@techkisan.in</a></li>
+	</ul>
+	 </div>
+	</div>
+	<div class="col-sm-3">
+	 <div class="footer_3">
+	  <h3>SEMPER PORTA</h3>
+	  <ul>
+           <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+           <li><a href="#"><i class="fa fa-twitter"></i></a></li> 
+           <li><a href="#"><i class="fa fa-google-plus"></i></a></li> 
+           <li><a href="#"><i class="fa fa-instagram"></i></a></li> 
+           		   
+	  </ul>
+	 </div>
+	</div>
+	<div class="col-sm-3">
+	 <div class="footer_2">
+	  <h3>VESTIBULUM</h3>
+	 </div>
+	 <div class="footer_4">
+	  <p>Enter your email and we’ll send you latest..</p>
+	 </div>
+	 <div class="footer_3">
+	  <p><input type="text" class="form-control" placeholder="Your Email Address"></p>
+	  <a href="#" class="button">SIGN UP</a>
+	 </div>	 
+	</div>
+   </div>
+  </div>
+ </div>
+ </div>
+</section>
+<section id="footer_main" class="clearfix">
+ <div class="col-sm-12">
+   <div class="footer_main_1">
+    <p>© 2013 Your Website Name. All Rights Reserved. Design by<a href="http://www.TemplateOnWeb.com"> Template On Web</a></p>
+   </div>
+ </div>
+</section>
+<script>
+$(document).ready(function(){
+	/*****Fixed Menu******/
+	var secondaryNav = $('.cd-secondary-nav'),
+	   secondaryNavTopPosition = secondaryNav.offset().top;
+		$(window).on('scroll', function(){
+			if($(window).scrollTop() > secondaryNavTopPosition ) {
+				secondaryNav.addClass('is-fixed');	
+			} else {
+				secondaryNav.removeClass('is-fixed');
+			}
+		});	
+});
+	</script>
+
+</body>
+
+	
+	
+	
+	
+
